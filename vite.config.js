5af00d8cfb4441d1293381d1b11e974c657917e8
Shanmuk Patnala <shanmukapatnala>
2026-08-12 21:30:03 +0530

    latest complete

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // Required for GitHub Pages
+  base: "/Project-Task-Manager/",
+
   server: {
     host: "localhost",
-    port: 3001,      // 🔥 completely different port
-    strictPort: true
-  }
+    port: 3001,
+    strictPort: true,
+  },
 });
